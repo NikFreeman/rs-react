757d6f48d981874bf6debf6f7155fa06ccc803ff
Nikolay Hancharonok <classic76@tut.by>
2023-03-13 14:30:07 +0300

    refactor: rename test folder

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import NotFound from './components/not-found';
 import About from './components/about';
+import Home from './components/home';
 import './index.css';
 
 const router = createBrowserRouter([
-  { path: '/', element: <div>Hello React</div> },
+  { path: '/', element: <Home /> },
   { path: '/about', element: <About /> },
   { path: '*', element: <NotFound /> },
 ]);
