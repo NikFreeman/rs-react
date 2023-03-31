c32b01696533c8260643c4fe69f9e0316be90480
Nikolay Hancharonok <classic76@tut.by>
2023-03-31 11:04:58 +0300

    refactor: add hooks from card and cardlist

--- src/components/card-list.tsx
+++ src/components/card-list.tsx
@@ -1,23 +1,16 @@
 import Card from './card';
-import { Component } from 'react';
 import data from '../data/products';
 import { Product } from '../models/products';
+import { useState } from 'react';
 
-interface ProductState {
-  products: Product[];
-}
+export default function CardList() {
+  const [state] = useState(data);
 
-export default class CardList extends Component<unknown, ProductState> {
-  state = { products: data };
-
-  render() {
-    const cards = this.state.products.map((item) => {
-      return <Card product={item} key={item.id} />;
-    });
-    return (
-      <div role="list" className="mx-auto flex max-w-5xl flex-row flex-wrap">
-        {cards}
-      </div>
-    );
-  }
+  return (
+    <div role="list" className="mx-auto flex max-w-5xl flex-row flex-wrap">
+      {state.map((item: Product) => {
+        return <Card product={item} key={item.id} />;
+      })}
+    </div>
+  );
 }
