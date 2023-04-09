dbd9ac5943277031c0fcdd5f3ba991e27cee525c
Nikolay Hancharonok <classic76@tut.by>
2023-04-09 10:29:11 +0300

    feat: add loading message

---
+++ src/components/loading.tsx
@@ -0,0 +1,8 @@
+function Loading() {
+  return (
+    <div className="">
+      <h1 className="text-center text-2xl font-bold text-green-600 ">Loading...</h1>
+    </div>
+  );
+}
+export default Loading;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,20 +3,26 @@ import CardList from '../components/card-list';
 import Search from '../components/search-bar';
 import loader from '../data/loader';
 import { Photo, SearchPhotos } from '../models/unsplash';
+import Loading from '../components/loading';
 
 export default function Home() {
   const [items, setItems] = useState<Photo[]>([]);
   const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const data = loader<SearchPhotos>(query);
-    data.then((items) => setItems(items.results));
+    data.then((items) => {
+      setItems(items.results);
+      setLoading(false);
+    });
   }, [query]);
 
   return (
     <div>
       <Search onSubmit={setQuery} />
-      <CardList items={items} />
+      {!loading ? <CardList items={items} /> : <Loading />}
     </div>
   );
 }
