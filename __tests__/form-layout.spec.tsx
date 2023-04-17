e2c4debdbef50870171ff4c97ce4865350fbfb29
Nikolay Hancharonok <classic76@tut.by>
2023-04-17 12:37:54 +0300

    fix: test form-layout

--- __tests__/form-layout.spec.tsx
+++ __tests__/form-layout.spec.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 
 describe('With React Testing Library', () => {
-  const initialState = { data: [] };
+  const initialState = { dataForm: { data: [] } };
   const mockStore = configureStore();
   let store;
   it('should render form', () => {
